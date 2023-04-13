65ef3b7cc1ae3c072ce4d7a7812f98e32f34eb9c
kdhg696 <kdhg696@astrazeneca.net>
2023-04-13 09:41:52 -0600

    Agregados comentarios en clase OtraClaseTest

--- src/test/java/clases/OtraClaseTest.java
+++ src/test/java/clases/OtraClaseTest.java
@@ -5,10 +5,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+// La clase debe tener el mismo nombre que la clase a probar y terminar con Test
 public class OtraClaseTest {
 
+    // Declarar objeto de la clase que vamos a probar
     OtraClase otraClase;
 
+    // El metodo de setup se usa para inicializar el objeto de la clase. Debe ser anotado con @BeforeEach
     @BeforeEach
     public void setup(){
         otraClase = new OtraClase();
@@ -17,6 +20,7 @@ public class OtraClaseTest {
     @DisplayName("Mandar un nombre valido. Debe retornar true")
     @Test
     public void esDeAR_nombrevalido_shouldReturnTrue(){
+        //Ademas de assertEquals hay muchos mas metodos de asercion https://junit.org/junit4/javadoc/4.8/org/junit/Assert.html
         Assertions.assertEquals(true, otraClase.esDeAR("Lupita"));
     }
 
